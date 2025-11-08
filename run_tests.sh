47f39ec1cd7ba0dce0d55e5462f9a76da8deec9f
Ruaan Deysel <ruaan.deysel@gmail.com>
2025-11-09 09:03:55 +1000

    Cleanup

--- run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Simple test runner script
-# Note: Test framework dependencies should be installed separately
-
-echo "Test files are located in the tests/ directory"
-echo "Run tests using your preferred testing framework"
